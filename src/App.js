9e80cb9f8a0093f41bf63d76d79b74b56771b77f
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-18 11:02:12 +0700

    add sideeffectcomp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import Navbar from './Components/NavbarComp/Navbar';
+// import { useState } from 'react';
+// import LifeCycleComp from './Components/LifeCycleComp/LifeCycleComp';
+import SideEffectComp from './Components/SideEffectComp/SideEffectComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navbar /> */}
+      <h1>Latihan SideEffect</h1>
+      <hr />
+      <SideEffectComp />
     </div>
   );
 }
